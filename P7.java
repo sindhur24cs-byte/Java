1ab8c21ea201ea54ee89ac16388b549f61c748cf
sindhur24cs-byte <sindhur24cs@rnsit.ac.in>
2026-02-11 17:00:32 +0530

    Fibonacci Series of a number

---
+++ P7.java
@@ -0,0 +1,43 @@
+//Find the Nth Fibonacci number using recursion.
+import java.util.Scanner;
+
+public class P7 {
+
+    static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println("Fibonacci = " + fibonacci(number));
+    }
+}
+//Fibonacci series up to N terms using a loop.
+import java.util.Scanner;
+
+public class P7 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
