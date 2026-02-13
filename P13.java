3dd263b12f34d9c446737f1aec4edea7fff35261
sindhur24cs-byte <sindhur24cs@rnsit.ac.in>
2026-02-13 12:13:02 +0530

    Finding the Prime Number

---
+++ P13.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class P13 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int count = 0;
+
+        if(num <= 1) {
+            System.out.println(num + " is not a Prime number");
+        } 
+        else {
+            for(int i = 1; i <= num; i++) {
+                if(num % i == 0) {
+                    count++;
+                }
+            }
+
+            if(count == 2) {
+                System.out.println(num + " is a Prime number");
+            } else {
+                System.out.println(num + " is not a Prime number");
+            }
+        }
+    }
+}
