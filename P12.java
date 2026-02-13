dfdbe2239d78e3657493256201c0d9fb0dac0070
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-02-13 11:36:23 +0530

    Add files via upload

---
+++ P12.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P12 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        int sum = 0;
+        
+        System.out.println("Enter " + n + " elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for(int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+        }
+        
+        System.out.println("Sum of all elements: " + sum);
+    }
+}
