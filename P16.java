1f153e6ab78bc798108f527c03503dc08423e770
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-02-17 11:50:38 +0530

    Find vowels or consonants

---
+++ P16.java
@@ -0,0 +1,28 @@
+
+public class P16 {
+
+    public static void main(String[] args) {
+
+        String text = "Hello World";
+        int vowels = 0, consonants = 0;
+
+        text = text.toLowerCase();
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+
+        System.out.println("Text: " + text);
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
