ccc2ff4962e12181a4cf53f8475e0e367ca0bba1
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-02-17 11:26:12 +0530

    Largest Number in array

---
+++ P3.java
@@ -0,0 +1,33 @@
+
+
+//take one array and find the largest number in the array.( taking the input nfrom user)
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = arr[0];
+
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest number is: " + largest);
+
+        sc.close();
+    }
+}
