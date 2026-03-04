fb04a6dec43a69344d92c9e4f2bfa930e63dfe5a
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-03-04 22:31:27 +0530

    Non repeating character in string

---
+++ P32.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FirstNonRepeat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("First non-repeating character: " + str.charAt(i));
+                return;
+            }
+        }
+
+        System.out.println("No non-repeating character found.");
+        sc.close();
+    }
+}
