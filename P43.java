4de6d5cc3e489250b68a08a4977dba0e8aab0d49
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-03-10 23:00:46 +0530

    Same Alaphabet Pattern

---
+++ P43.java
@@ -0,0 +1,16 @@
+
+public class P43
+{
+	public static void main(String[] args) {
+		
+		for(int i = 1;i<=4;i++){
+		    char ch = (char)('A'+i-1);
+	
+		for(int j = 1;j<=i;j++){
+		    System.out.print(ch +" ");
+		    
+		}
+		System.out.println();
+	}
+}
+}
