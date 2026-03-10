b2326471900871bb7a124ae89124f9e6ed30abde
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-03-10 23:06:33 +0530

    Reverse Triangle Pattern

---
+++ P47.java
@@ -0,0 +1,16 @@
+
+public class P47
+{
+	public static void main(String[] args) {
+	    for(int i = 4;i>=1;i--){
+	    char ch = 'A';
+		    
+		for(int j = 1;j<=i;j++){
+		    System.out.print(ch +" ");
+		    ch++;
+		    
+		}
+		System.out.println();
+	}
+}
+}
