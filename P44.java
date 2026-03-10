405890a6fec38724be35072e6754e00f8f6fe739
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-03-10 22:57:29 +0530

    Alphabet Triangle

---
+++ P44.java
@@ -0,0 +1,15 @@
+
+public class P44
+{
+	public static void main(String[] args) {
+		
+		for(int i = 1;i<=4;i++){
+		char ch = 'A';
+		for(int j = 1;j<=i;j++){
+		    System.out.print(ch +" ");
+		    ch++;
+		}
+		System.out.println();
+	}
+}
+}
