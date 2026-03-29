b5be20b4fad0d0aaec093a3c38eeb8d1c27ea5a9
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-03-29 23:53:59 +0530

    Print msg

---
+++ P58.java
@@ -0,0 +1,6 @@
+public class ReunionPoll {
+    public static void main(String[] args) {
+        String message = "12 years later... let’s meet in Chikkamagaluru. Will you join?";
+        System.out.println(message);
+    }
+}
