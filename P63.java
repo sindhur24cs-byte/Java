0bb57cf6f999152b25b1d1bbdc5018313a21d8a6
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-04-08 22:04:18 +0530

    Selection sort

---
+++ P63.java
@@ -0,0 +1,29 @@
+class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        int n = arr.length;
+
+        // Selection Sort logic
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        // Print sorted array
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
