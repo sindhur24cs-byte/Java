b92f808e0cdfdf49425ad85d9c0dcfa50934e46d
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-04-21 23:58:16 +0530

    Unique

---
+++ P69.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class UniqueElements {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        Set<Integer> unique = new LinkedHashSet<>();
+        for (int num : arr) {
+            unique.add(num);
+        }
+
+        System.out.println("Unique elements: " + unique);
+    }
+}
