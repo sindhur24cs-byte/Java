ef8f30d12d87a91b44566a86c9a9d8e7e2d5fcca
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-06-16 00:00:11 +0530

    Create P86.java

---
+++ P86.java
@@ -0,0 +1,9 @@
+public class GoodMorning {
+    public static void main(String[] args) {
+        int[] ascii = {71,111,111,100,32,77,111,114,110,105,110,103,32,83,105,114};
+
+        for (int i : ascii) {
+            System.out.print((char)i);
+        }
+    }
+}
