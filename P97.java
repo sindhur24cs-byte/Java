ef8f221f5f25a7eb654d937c1698fd606d5b86be
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-06-21 23:52:37 +0530

    Array list

---
+++ P97.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListAddition {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Adding elements
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.println("ArrayList elements: " + list);
+    }
+}
