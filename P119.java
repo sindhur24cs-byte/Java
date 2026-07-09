f3ece3da79df16c9bdba148e33b8b02eea870576
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-09 23:03:37 +0530

    Rotate Array by k position

---
+++ P119.java
@@ -0,0 +1,19 @@
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int k = 2;
+
+        k = k % arr.length;
+
+        for(int i=0;i<k;i++){
+            int last = arr[arr.length-1];
+            for(int j=arr.length-1;j>0;j--){
+                arr[j]=arr[j-1];
+            }
+            arr[0]=last;
+        }
+
+        for(int x:arr)
+            System.out.print(x+" ");
+    }
+}
