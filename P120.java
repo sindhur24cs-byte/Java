10e462158dd8da50e0c8fb79d158d3db623b535e
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-07-14 06:37:55 +0530

    Power recursion

---
+++ P120.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PowerRecursion {
+
+    static int power(int base, int exp) {
+        if (exp == 0)
+            return 1;
+        return base * power(base, exp - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base: ");
+        int base = sc.nextInt();
+
+        System.out.print("Enter exponent: ");
+        int exp = sc.nextInt();
+
+        System.out.println("Power = " + power(base, exp));
+    }
+}
